68ca7dac6cbcb68cccd2a37b5d49943b3c8f37e6
taKana671 <kanae5321@gmail.com>
2026-07-13 01:23:11 +0900

    Added RoguePlanet class.

--- galaxy/__init__.py
+++ galaxy/__init__.py
@@ -3,3 +3,4 @@ from .galaxy import Galaxy
 from .lights import GalaxyAmbientLight, SunPointLignt
 from .planet import Planet, OrbitLine, ParticleRing, Atmosphere
 from .sun import Sun, SolarFlare
+from .rogue_planet import RoguePlanet
